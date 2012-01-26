978b2936977a7a397cd0b6f85eb3b0ede9d4db1c
Janus D <sunajd@googlemail.com>
2012-01-27 00:39:00 +0100

    first commit

---
+++ src/de/druz/run/Run.java
@@ -0,0 +1,29 @@
+package de.druz.run;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Run extends BasicGame {
+
+	public Run(String title) {
+		super("Run");
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+
+	}
+
+}
